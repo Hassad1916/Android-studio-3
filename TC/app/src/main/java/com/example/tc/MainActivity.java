38334f25a894b81855953327dc1fe3c1dd0c2615
Hassad1916 <72906611+Hassad1916@users.noreply.github.com>
2020-11-12 14:45:18 +0800

    Temperature conversion

---
+++ TC/app/src/main/java/com/example/tc/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.tc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat nf = new DecimalFormat("0.00");//小數後兩位
+                EditText tmpc = (EditText)findViewById(R.id.editTextTextPersonName);
+                //華氏
+                double c = Double.parseDouble(tmpc.getText().toString());
+
+                double f = (c * 9 / 5) + 32;
+
+                TextView trans_Ans = (TextView)findViewById(R.id.textView4);
+                trans_Ans.setText(""
+                        + nf.format(f) + " F");
+            }
+        });
+
+        Button button1 = (Button)findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat nf = new DecimalFormat("0.00");//小數後兩位
+                EditText tmpf = (EditText)findViewById(R.id.editTextTextPersonName);
+                //攝氏
+                double f = Double.parseDouble(tmpf.getText().toString());
+
+                double c = (f - 32) * 5 / 9;
+
+                TextView trans_Ans = (TextView)findViewById(R.id.textView4);
+                trans_Ans.setText(""
+                        + nf.format(c)+" C");
+            }
+        });
+
+    }
+
+}
